86b971738f7bedda59237e452a01fc34b5774e2c
madhumithabalaji <32056635+madhumithabalaji@users.noreply.github.com>
2021-02-04 21:41:02 -0500

    Update simpleAdd.java

--- simpleAdd.java
+++ simpleAdd.java
@@ -1,9 +1,5 @@
 import java.io.*;
-import java.lang.reflect.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 class Solution {
     public static void main(String[] args) {
